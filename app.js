83a4bb2f73bd75b48755fca13e67baead382cf87
Aagii <agii_0930g@yahoo.com>
2023-11-20 13:28:29 +0900

    toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -26,9 +26,7 @@ var roundScore = 0;
 // document.querySelector("#score-0").textContent = 0;
 
 document.getElementById("score-0").textContent = 0;
-
 document.getElementById("score-1").textContent = 0;
-
 document.getElementById("current-0").textContent = 0;
 document.getElementById("current-1").textContent = 0;
 
@@ -39,9 +37,38 @@ var diceDom = document.querySelector(".dice");
 // Anonymous bichiglel
 document.querySelector(".btn-roll").addEventListener("click", function () {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
-  //   document.querySelector(".dice").style.display = "block";
+  document.querySelector(".dice").style.display = "block";
   diceDom.src = "dice-" + diceNumber + ".png";
   //   alert("Shoog shidlee : " + diceNumber);
-});
 
-// console.log("Dice :" + diceNumber);
+  // Buusan too ni 1-ees ylgaatai bol idewhtei toglogchiin eeljiin onoog nemegduulne
+  if (diceNumber !== 1) {
+    // 1-ees ylgaatai too buulaa. Buusan toog toglogchid nemj ugnu
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // 1 buusan tul toglogchiin eeljiig solij ugnu
+
+    // Ene toglogchiin eeljin deer tsugluulsan onoog 0 bolgono
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    // Toglogchiin eeljiig nuguu toglogch ruu shiljuulne
+    // herew idewhtei toglogch ni 0 baival idevhtei toglogchiig 1 bolgo
+    // Ugui bol idewhtei toglogchiig 0 bolgo
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    // Active panel-iig daraagiin toglogch ruu shiljihed panel-iig solih
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // Shoog  tur alga bolgoh
+    diceDom.style.display = "none";
+
+    // if (activePlayer === 0) {
+    //   activePlayer = 1;
+    // } else {
+    //   activePlayer = 0;
+    // }
+  }
+});
